c31d692a86c74de4ef2ac8353b51c54a656e08e2
Rohan Bawa <rohanbawa11@gmail.com>
2026-01-13 19:38:43 -0500

    deployment prep and changes

--- backend/index.js
+++ backend/index.js
@@ -122,7 +122,9 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 4000;
+
+const PORT = process.env.PORT || 4000;
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- frontend/socket.js
+++ frontend/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-// Point this to your backend URL
-const URL = "http://localhost:4000";
 
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
 export const socket = io(URL, {
   autoConnect: false,
 });
